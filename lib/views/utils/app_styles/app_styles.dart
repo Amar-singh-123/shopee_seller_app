34308da3f5b3836d0f0efcfd5e9633d34b070c04
priyanshuuuyadav <priyanshuuuyadav@gmail.com>
2024-03-28 14:44:46 +0530

    heyy i'm adding a order section

--- lib/views/utils/app_styles/app_styles.dart
+++ lib/views/utils/app_styles/app_styles.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:shopee_seller_app/views/utils/app_colors/app_colors.dart';
 
  TextStyle defaultTextStyle = TextStyle(color: AppColor.white);
+ TextStyle appBarTextStyle = TextStyle(
+     color: Colors.black, fontWeight: FontWeight.w500, fontSize: 19);
 ButtonStyle elevatedButtonStyle = ElevatedButton.styleFrom(
     backgroundColor: AppColor.blueAccent,
     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)));
