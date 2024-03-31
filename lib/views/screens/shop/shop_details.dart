8200a7fae9764014396dae6f554dc85122bcfb67
Amar-singh-123 <125099473+Amar-singh-123@users.noreply.github.com>
2024-03-31 15:21:54 +0530

    i added a a profile sec

--- lib/views/screens/shop/shop_details.dart
+++ lib/views/screens/shop/shop_details.dart
@@ -4,7 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:image_picker/image_picker.dart';
-import 'package:shopee_seller_app/model/seller_details_model.dart';
+import 'package:shopee_seller_app/models/seller_details_model.dart';
 import 'dart:io';
 import 'package:shopee_seller_app/views/utils/app_extensions/app_extensions.dart';
 
