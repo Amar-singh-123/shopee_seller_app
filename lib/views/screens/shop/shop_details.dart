c91b4bd6e9c7c508e048e2ed7b64bee667cb0202
Amar-singh-123 <125099473+Amar-singh-123@users.noreply.github.com>
2024-04-02 02:38:40 +0530

    i configered files

--- lib/views/screens/shop/shop_details.dart
+++ lib/views/screens/shop/shop_details.dart
@@ -6,18 +6,17 @@ import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:image_picker/image_picker.dart';
 import 'package:shopee_seller_app/models/shopModel/shop_model.dart';
 import 'dart:io';
-import 'package:shopee_seller_app/views/screens/shop_details/setting_screen.dart';
 import 'package:shopee_seller_app/views/utils/app_extensions/app_extensions.dart';
 import '../settings/setting_screen.dart';
 
-class SellerDetails extends StatefulWidget {
-  const SellerDetails({Key? key});
+class ShopDetailsScreen extends StatefulWidget {
+  const ShopDetailsScreen({Key? key});
 
   @override
-  State<SellerDetails> createState() => _SellerDetailsState();
+  State<ShopDetailsScreen> createState() => _ShopDetailsScreenState();
 }
 
-class _SellerDetailsState extends State<SellerDetails> {
+class _ShopDetailsScreenState extends State<ShopDetailsScreen> {
   TextEditingController storeNameController = TextEditingController();
   TextEditingController businessNameController = TextEditingController();
   TextEditingController phoneController = TextEditingController();
